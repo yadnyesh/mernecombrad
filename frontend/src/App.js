8cd6a706c4ab5918e2d41dd9f56f533bfa91a143
y0j01bf <yadnyesh.juvekar@walmartlabs.com>
2020-11-12 20:12:32 +0530

    Section2: Chap5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <Header />
       <Container>
-        <main>
+        <main className='py-3'>
           <h1>Welcome to ProShop!</h1>
         </main>  
       </Container>   
